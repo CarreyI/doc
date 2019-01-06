e945f76409fe57db22e4908b4ce8a535d0b35085
ThanJu <466188665@qq.com>
2019-01-06 12:01:56 +0800

    动态窗口tab选中

--- phoibe-doc-cms-web/src/main/resources/templates/js/sonCunkLoad.js
+++ phoibe-doc-cms-web/src/main/resources/templates/js/sonCunkLoad.js
@@ -1,84 +1,158 @@
-function appendCunkHtml(){
-    var dataDict = parent.dataDictLoadAjax();
 
-    //sonCunkLoad(dataDict);
+var dataDictHtml;
+function appendCunkHtml(){
+    var userCunkObje= [
+        {USER_CONFIG:"WARSTATE",WARS_TACTICS:1,QUERYWHERE:"美国"},
+        {USER_CONFIG:"WARNUM",WARS_TACTICS:1,QUERYWHERE:"1~5万"},
+        {USER_CONFIG:"WARADDR",WARS_TACTICS:1,QUERYWHERE:"平原作战"},
+        {USER_CONFIG:"WARSTYPE",WARS_TACTICS:1,QUERYWHERE:"海陆联合战例"},
+        {USER_CONFIG:"FIGHTTYPE",WARS_TACTICS:2,QUERYWHERE:"战术战法"},
+        {USER_CONFIG:"COMBATTYPE",WARS_TACTICS:2,QUERYWHERE:"袭击战"},
+        {USER_CONFIG:"CORPSTYPE",WARS_TACTICS:2,QUERYWHERE:"空军作战"},
+        {USER_CONFIG:"FIGHTTRAIT",WARS_TACTICS:2,QUERYWHERE:"以少胜多"}
+        ];
+
+    var sonCunkHtml = sonCunkLoad(userCunkObje);
+    $("#zhanli-ul").html(sonCunkHtml.zl);
+    $("#zhanfa-ul").html(sonCunkHtml.zf);
+
 }
 function sonCunkLoad(obj){
 
-    var chunkhtml="";
-    $.each(obj, function (i, val) {
-        var field=selectfield(obj[i]);
-        var doclist=cunkListLoad(val["doclist"]);
-        var row="<li class='sonChunk'>" +
-            "<div class='chunk_title'>" +
-            "<span class='label'>作战年代</span>" +
-            "</div>" +
-            "<div class='chunk_select'>" +
-            "<select  name=''>" +
-            "<option class='tag-li' value='89'>美国</option>" +
-            "</select>" +
-            "</div>" +
-            "<div class='chunk_content'>" +
-            "<div class='dynamiclist'>" +
-            " <ul>"+doclist+"</ul></div></div></li>";
-        chunkhtml = chunkhtml+ row;
+    var chunkHtmlObj={};
+    $.each(obj, function (i, val) {;
+        var WARS_TACTICS = val.WARS_TACTICS
+        var USER_CONFIG = val.USER_CONFIG
+        var QUERYWHERE = val.QUERYWHERE
+
+        var fieldObje = selectfield(USER_CONFIG);//将数据字典中的字段名转为映射类中的属性名
+
+        //取出方块的字段名（对应映射类中属性名）和默认查询参数查询
+        var dataObj=getDocFieldList(fieldObje.fn,QUERYWHERE);
+        var field=fieldObje.ft;
+        var doclist=cunkListLoad(dataObj.datalist);
+        var widnum = dataObj.datacount;
+        var row="<div class='wid-border' >" +
+            "   <div class='wid-cicle'>" +
+            "            <div class='wid-title'>"+field+"</div>" +
+            "            <div class='wid-num'>"+widnum+"</div>" +
+            "   </div>" +
+            "   <div class='wid-title-bk'>"+
+                    "<select  name='"+USER_CONFIG+"'>" +
+                    dataDictHtml[USER_CONFIG]+
+                    "</select>" +
+            "   </div>" +
+            "   <div class='dynamiclist'>" +
+                doclist+
+            "   </div>"+
+            " </div>";
+
+        if (WARS_TACTICS==1){
+            if(null!=chunkHtmlObj.zl){
+                var htmlobj = chunkHtmlObj.zl;
+                htmlobj+=row;
+                chunkHtmlObj.zl = htmlobj;
+            }else{
+                chunkHtmlObj.zl = row;
+            }
+        }else{
+            if(null!=chunkHtmlObj.zf){
+                var htmlobj = chunkHtmlObj.zf;
+                htmlobj+=row;
+                chunkHtmlObj.zf = htmlobj;
+            }else{
+                chunkHtmlObj.zf = row;
+            }
+        }
+        //alert(JSON.stringify(row))
     })
-    return chunkhtml;
+    return chunkHtmlObj;
 }
 function cunkListLoad(obj){
     var listhtml = "";
     $.each(obj, function (i, val) {
+        var tid = val["id"];
         var format = val["format"];
         var docname = val["name"];
-        var createTime = val["stockTime"];
-        var tid = val["id"];
+        var stockTime = val["stockTime"];
+        if (null!=stockTime){
+            stockTime = stockTime.substring("5","10");
+        }
         var description=val["description"];
+        if (null!=description){
+            description = cutString(description,76);
+        }
         var hrefUrl= "docdetail.html?tid=" + tid + "' title='" + docname + "'";
         var row="<li class='right-item'><a href='"+hrefUrl+"'target='_blank'>"+
             "<div class='right-item-content clearfix'><h5 class='' title='"+docname+"'>"+cutString(docname,24)+
-            "<span class='time'>&nbsp;&nbsp;&nbsp;&nbsp;"+createTime.substring("5","10")+"</span></h5></div>"+
-            "<div class='right-item-desc'>"+cutString(description,76)+"</div>"+
+            "<span class='time'>&nbsp;&nbsp;&nbsp;&nbsp;"+stockTime+"</span></h5></div>"+
+            "<div class='right-item-desc'>"+description+"</div>"+
             "</a></li>";
-        listhtml= listhtml+row;
+        listhtml+=row;
     });
     return listhtml;
 }
 function selectfield(field){
-    var fieldname="";
+    var fieldname="";//在映射类中的字段名，查询用
+    var fieldtitle="";
     switch (field) {
         case "WARSTATE" :
-            fieldname= "参战国家";
+             fieldtitle= "参战国家";
+            fieldname= "warstate";
             break;
         case "WARNUM" :
-            fieldname= "作战规模";
+             fieldtitle= "作战规模";
+            fieldname= "warnum";
             break;
         case "WARADDR" :
-            fieldname= "作战地域";
+             fieldtitle= "作战地域";
+            fieldname= "waraddr";
             break;
         case "WARSTYPE" :
-            fieldname= "战例类型";
+             fieldtitle= "战例类型";
+            fieldname= "warstype";
             break;
         case "CORPSTYPE" :
-            fieldname= "兵种类型";
+             fieldtitle= "兵种类型";
+            fieldname= "corpstype";
             break;
         case "FIGHTTIME" :
-            fieldname= "作战年代";
+             fieldtitle= "作战年代";
+            fieldname= "fighttime";
             break;
         case "FIGHTTYPE" :
-            fieldname= "战法类别";
+             fieldtitle= "战法类别";
+            fieldname= "fighttype";
             break;
         case "COMBATTYPE" :
-            fieldname= "战斗种类";
+             fieldtitle= "战斗种类";
+            fieldname= "combattype";
             break;
         case "FIGHTTRAIT" :
-            fieldname= "作战特点";
+             fieldtitle= "作战特点";
+            fieldname= "fighttrait";
             break;
-        case "DOCTYPE" :
-            fieldname= "文档类别";
             break;
     }
-    return fieldname;
+
+    return {fn:fieldname,ft:fieldtitle};
+}
+function getDocFieldList(dield,querywhere){
+    var data = "phoibe/document/list/user/0/5?&auditStatus=2&&isstock=2&"+dield+"="+querywhere;
+    var listObjt={};
+    $.ajax({
+        type: 'GET',
+        url: GAL_URL + data,
+        async: false,
+        dataType: 'json',
+        success: function (result) {
+            listObjt.datacount = result.data.totalCount;
+            listObjt.datalist = result.data.dataList
+        }
+    });
+    return listObjt;
 }
 $(function () {
+    dataDictHtml = parent.dataDictSelectHtml();
     appendCunkHtml()
 })
